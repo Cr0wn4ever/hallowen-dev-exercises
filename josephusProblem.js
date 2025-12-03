cce9d4656acf81e78ab184e80ee3b000443e150f
Victor Patino III <127069661+Cr0wn4ever@users.noreply.github.com>
2025-12-03 11:53:20 -0400

    Add Josephus problem solution in josephusProblem.js
    Implement Josephus problem solution with iterative and recursive functions.

---
+++ josephusProblem.js
@@ -0,0 +1,25 @@
+// EJERCICIO 6 EL PROBLEMA DE JOSEFO
+
+function surviveRoulette(n, k) {
+  const survivor = [...Array(n).keys()];
+  while (survivor.length > 1) {
+    for (let i = 0; i < k - 1; i++) {
+      const salvados = survivor.shift();
+      survivor.push(salvados);
+    }
+    const eliminado = survivor.shift();
+  }
+  return survivor;
+}
+
+function surviveRouletteRecursive(n, k) {
+  const survivor = [...Array(n).keys()];
+  if (n == 1) {
+    return 0;
+  }
+
+  return (surviveRouletteRecursive(n - 1, k) + k) % n;
+}
+
+console.log(`Sin recursividad: EL SOBREVIVIENTE ES ${surviveRoulette(4, 2)}`)
+console.log(`Con recursividad: EL SOBREVIVIENTE ES ${surviveRouletteRecursive(4,2)}`)
